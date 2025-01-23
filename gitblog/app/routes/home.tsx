ee1eef65e9116f0ceff056406aeed8c62be10221
seojaeoh <seojaeohcoder@gmail.com>
2025-01-23 19:32:25 +0900

    create project

---
+++ gitblog/app/routes/home.tsx
@@ -0,0 +1,18 @@
+import type { Route } from "./+types/home";
+import { Welcome } from "../welcome/welcome";
+import { Hello } from "../welcome/hello";
+
+
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return (
+    <Hello />
+  );
+}
---
+++ gitblog/app/welcome/Hello.tsx
@@ -0,0 +1,5 @@
+export function Hello() {
+  return (
+    <div>H!</div>
+  );
+}
