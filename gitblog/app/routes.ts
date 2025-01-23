21560013f48e74b955b5c12fe8d78ac14f739b8a
seojaeoh <seojaeohcoder@gmail.com>
2025-01-24 06:50:52 +0900

    reset

--- gitblog/app/routes.ts
+++
@@ -1,6 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-    index("routes/home.tsx"),
-    route("post/:filename", "routes/post.tsx")
-] satisfies RouteConfig;
